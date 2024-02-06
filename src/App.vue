15084f99f41b7dfeeba19abcebff3e2a7f3ee0ef
Julien <julien.thoumy@gmail.com>
2024-02-06 23:25:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SideMenu from './components/SideMenu.vue';
+
+</script>
+
+<template>
+  <div id="SideNav" class="fixed w-[240px] bg-[#191922] h-[100vh] border-r border-r-[#32323D]">
+    <div class="mt-[80px]"></div>
+    <SideMenu pageUrl="/" name="Accueil"/>
+    <SideMenu pageUrl="/" name="À propos de moi"/>
+    <SideMenu pageUrl="/" name="Portfolio"/>
+    <SideMenu pageUrl="/" name="Contact"/>
+    <RouterView/>
+  </div>
+</template>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,25 @@
+    <script setup>
+    import { toRefs, defineProps } from 'vue'
+    import { RouterLink, useRoute } from 'vue-router';
+    
+    const route = useRoute()
+    
+    const props = defineProps({
+        pageUrl: String,
+        name: String
+    })
+    const { pageUrl, name } = toRefs(props)
+    </script>
+<template>
+    <div class="flex items-center w-full my-[20px]">
+        <RouterLink
+            :to="pageUrl"
+            :class="pageUrl === route.path ? 'border-l-[#EF5465] text-[#EF5465]' : 'border-l-[#191922] text-[#FFFFFF]'"
+            class="border-l-4 w-full hover:text-[#EF5465]"
+        >
+            <div class="flex items-center pl-3 mx-3 cursor-pointer">
+                <div class="pl-3.5 font-[600] text-white text-[17px]">{{ name }}</div>
+            </div>
+        </RouterLink>
+    </div>
+</template>
